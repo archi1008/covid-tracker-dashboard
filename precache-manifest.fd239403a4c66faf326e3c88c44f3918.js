09c8896aa572296f39982345b49bb5882667fc35
archi1008 <archiaa.1008@gmail.com>
2022-04-10 10:51:26 +0530

    Updates

---
+++ precache-manifest.fd239403a4c66faf326e3c88c44f3918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973ccba1b5df37b8db88e6e98eac3af3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85974ef44adb1fc0a8e",
+    "url": "/static/css/main.01d07af7.chunk.css"
+  },
+  {
+    "revision": "b6742956d0d73730b709",
+    "url": "/static/js/2.ea59d149.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ea59d149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85974ef44adb1fc0a8e",
+    "url": "/static/js/main.d304ec37.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  }
+]);
